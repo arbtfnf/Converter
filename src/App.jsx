eee3153d3353d27df04c935de79d9ae6e4077f63
anuragbandhu <anuragbandhu007@gmail.com>
2023-10-04 01:48:30 +0530

    added copy to clipboard and reset functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState, useRef } from 'react'
 import Input from './components/input'
 import useConverter from './hooks/useConverter'
 
@@ -10,6 +10,8 @@ function App() {
   const [convertedAmount, setConvertedAmount] = useState(0)
 
   const currencyInfo = useConverter(from)
+
+  const textRef = useRef(null);
 
   const options = Object.keys(currencyInfo)
 
@@ -25,6 +27,18 @@ function App() {
     setConvertedAmount(amount * currencyInfo[to])
   }
 
+  const resetValues = () => {
+    setAmount(0)
+    setConvertedAmount(0)
+  }
+
+  const copyTextToClipboard = useCallback(() => {
+    textRef.current?.select()
+    window.navigator.clipboard.writeText(convertedAmount);
+  }, [convertedAmount])
+  
+
+
   return (
     <div
         className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
@@ -36,6 +50,7 @@ function App() {
         >
             <div className="w-full">
                 <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                  <h1>Currency Converter</h1>
                     <form
                         onSubmit={(e) => {
                             e.preventDefault();
@@ -59,7 +74,7 @@ function App() {
                                 className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                                 onClick={swap}
                             >
-                                swap
+                              swap
                             </button>
                         </div>
                         <div className="w-full mt-1 mb-4">
@@ -70,11 +85,33 @@ function App() {
                                 onCurrencyChange={(currency) => setTo(currency)}
                                 selectCurrency={to}
                                 amountDisable
+                                ref={textRef}
                             />
                         </div>
                         <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
                             Convert {from.toUpperCase()} to {to.toUpperCase()}
                         </button>
+                        
+                        <div className='flex py-2 p-2'>
+                          <button 
+                            onClick={copyTextToClipboard}
+                            type="submit" 
+                            className="relative w-full bg-blue-100
+                            text-slate-500 px-4 py-3 rounded-lg
+                            hover:bg-slate-200"
+                          >
+                            Copy amount {from.toUpperCase()} to {to.toUpperCase()}
+                          </button>
+                          <button 
+                            onClick={resetValues}
+                            type="submit" 
+                            className="relative w-small bg-blue-100
+                          text-slate-500 px-4 py-3 rounded-lg
+                          hover:bg-slate-200"
+                          >
+                            Reset
+                          </button>
+                        </div>
                     </form>
                 </div>
             </div>
